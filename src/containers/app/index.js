8338fa1ffdb8c38a293daadf5b7a03575f6ed57f
leonardopliski <leonardopliski@gmail.com>
2019-03-20 01:08:15 -0300

    Modify Provider file structure

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
 
 // importing our containers
 import TodosContainer from "../todos";
 
-const App = () => {
+const App = ({ store }) => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact={true} component={TodosContainer} />
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact={true} component={TodosContainer} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import "antd/dist/antd.css";
 import App from "./containers/app";
@@ -13,12 +12,7 @@ import "./global.scss";
 // root node to render our application
 const target = document.getElementById("root");
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  target
-);
+render(<App store={store} />, target);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
