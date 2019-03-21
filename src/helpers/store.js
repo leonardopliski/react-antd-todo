41ebe7a0d838d92c62d11fe7b9d7812affdd3933
leonardopliski <leonardopliski@gmail.com>
2019-03-21 13:34:18 -0300

    Add comments to store.js

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -1,17 +1,23 @@
 import { createStore } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+
+// importing root application reducer
 import rootReducer from "../reducers";
 
+// Redux Persis Config
 const persistConfig = {
   key: "application",
   storage: storage,
   stateReconciler: autoMergeLevel2
 };
 
+// Persisting our reducer
 const pReducer = persistReducer(persistConfig, rootReducer);
 
+// creating our store with the persistant reducer
 export const store = createStore(pReducer);
+
+// persisting our store to the next time that page reloads
 export const persistor = persistStore(store);
