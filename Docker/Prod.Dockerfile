8e8796c1ea62691e260c09ac1614faa9d104d0ff
leonardopliski <leonardopliski@gmail.com>
2020-03-19 23:27:52 -0300

    build: add new test packages & upgrade
    * add latest nginx version

--- Docker/Prod.Dockerfile
+++ Docker/Prod.Dockerfile
@@ -13,7 +13,7 @@ COPY . ./
 
 RUN yarn build
 
-FROM nginx:1.17.9-alpine
+FROM nginx:alpine
 
 COPY --from=builder /usr/src/app/build /usr/share/nginx/html
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import * as serviceWorker from "./serviceWorker";
 
 import { store, persistor } from "./helpers";
 
@@ -13,8 +12,3 @@ import App from "./containers/app";
 const target = document.getElementById("root");
 
 render(<App store={store} storePersistor={persistor} />, target);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
