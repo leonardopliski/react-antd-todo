7258a23486497cf545454caf3e6e1c33423aa35b
leonardopliski <leonardopliski@gmail.com>
2020-04-16 23:32:20 -0300

    fix(theme): add temporary light theme * Upgrade antd

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,12 @@
-const { override, fixBabelImports, addLessLoader } = require("customize-cra");
-const darkThemeVars = require("antd/dist/dark-theme");
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
-  fixBabelImports("import", {
-    libraryName: "antd",
-    style: true
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    style: true,
   }),
 
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: {
-      hack: `true;@import "${require.resolve(
-        "antd/lib/style/color/colorPalette.less"
-      )}";`,
-      ...darkThemeVars,
-      "@primary-color": "#13c2c2",
-      "@text-color": "#FFFFFF"
-    }
   })
 );
