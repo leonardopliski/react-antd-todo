593d1761e5ac533a6be42e7e4e65e8905428d681
leonardopliski <leonardopliski@gmail.com>
2020-06-02 22:35:18 -0300

    build(packages): update package versions

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,14 @@
 const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
+  fixBabelImports('antd', {
+    libraryDirectory: 'es',
     style: true,
   }),
 
   addLessLoader({
-    javascriptEnabled: true,
+    lessOptions: {
+      javascriptEnabled: true,
+    },
   })
 );
