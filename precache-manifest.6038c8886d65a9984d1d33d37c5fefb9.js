5a20dea926b2464d782e538e2950e54e5a421c7a
leonardopliski <leonardopliski@gmail.com>
2020-06-03 01:50:52 +0000

    Deploying to gh-pages from  @ 0232d531d12afe9938b9993ac3ee07a8d216698a 🚀

---
+++ precache-manifest.6038c8886d65a9984d1d33d37c5fefb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533fb04bef7d617b4564b0d240115116",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43ec87c17f9ab8c53c8f",
+    "url": "/static/css/2.9801f77b.chunk.css"
+  },
+  {
+    "revision": "6fd31fcec89122b0908c",
+    "url": "/static/css/main.aaa8e670.chunk.css"
+  },
+  {
+    "revision": "43ec87c17f9ab8c53c8f",
+    "url": "/static/js/2.c6b9b62b.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/static/js/2.c6b9b62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd31fcec89122b0908c",
+    "url": "/static/js/main.eb351062.chunk.js"
+  },
+  {
+    "revision": "247ef960beaa32fa1907",
+    "url": "/static/js/runtime-main.328bb17e.js"
+  }
+]);
